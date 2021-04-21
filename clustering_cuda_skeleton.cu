9b2d4da64fdbd34992026c9c0e2740c0cc27bb8c
bella meng <dancingm1998@gmail.com>
2021-04-21 15:30:49 +0800

    find pivots added debug code

--- clustering_cuda_skeleton.cu
+++ clustering_cuda_skeleton.cu
@@ -38,8 +38,8 @@ void findPivots(int num_blocks_per_grid, int num_threads_per_block, int num_vs, 
     int element_skip = blockDim.x * gridDim.x;
     
     for (int i = tid; i < num_vs; i += element_skip) {
-        int left_start = d_nbrs[d_nbr_offs[i]];
-        int left_end = d_nbrs[d_nbr_offs[i + 1]];
+        int left_start = d_nbr_offs[i];
+        int left_end = d_nbr_offs[i + 1];
         int left_size = left_end - left_start;
 #if __CUDA_ARCH__>=200
         if (tid < 17)
